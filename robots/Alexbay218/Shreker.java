c4913343cc1729012db9c93f8a13a81dbda46e48
Shaun Wu <Alexbay218@gmail.com>
2016-03-08 13:09:15 -0500

    Linear targeting and virtual gun improved with distance management

--- robots/Alexbay218/Shreker.java
+++ robots/Alexbay218/Shreker.java
@@ -7,17 +7,19 @@ import java.awt.Color;
 public class Shreker extends AdvancedRobot
 {
 	/**
-	 * run: Shreker's default behavior
+	 * run: Shreker`s default behavior
 	 */
+	private int direction = 1;
+	private double enemyLife = 100;
+	private double angle = 90;
 	public void run() {
+		System.out.println("Shrek is love, ");
 		setAdjustGunForRobotTurn(true);
 		setAdjustRadarForRobotTurn(true);
 		setAdjustRadarForGunTurn(true);
 		setColors(Color.green,Color.green,Color.green); // body,gun,radar
-		turnRadarRight(360);
 		while(true) {
-			turnRadarRight(5);
-			turnRadarLeft(10);
+			turnRadarLeft(30);
 		}
 	}
 
@@ -26,15 +28,54 @@ public class Shreker extends AdvancedRobot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		double turn = (getHeading() + e.getBearing() - getGunHeading());
-		double radarTurn = getHeading() + e.getBearing() - getRadarHeading();
+		double radarTurn = (getHeading() + e.getBearing()) - getRadarHeading();
 		if(turn > 180) {
-			turn = 360 - turn;
+			turn = turn - 360;
+		}
+		else if(turn < -180) {
+			turn = 360 + turn;
 		}
 		if(radarTurn > 180) {
-			radarTurn = 360 - radarTurn;
+			radarTurn = radarTurn - 360;
 		}
-		turnRadarRight(radarTurn);
+		else if(radarTurn < -180) {
+			radarTurn = 360 + radarTurn;
+		}
+		turnRadarRight(radarTurn+30);
 		turnGunRight(turn);
-		fire(1);
-	}	
+		if(getEnergy() + 10 >= e.getEnergy()) {
+			turnGunRight(Math.toDegrees(Math.asin(e.getVelocity()*Math.cos(Math.toRadians(getHeading()-e.getHeading()))/11)));
+		}
+		fire(3);
+		angle = 90 - direction * e.getDistance()/180;
+		turnRight(e.getBearing()+angle);
+		setAhead(30*direction);
+		if(enemyLife > e.getEnergy()) {
+			enemyLife = e.getEnergy();
+			setColors(Color.green,Color.yellow,Color.green);
+			direction *= -1;
+		}
+		else {
+			setColors(Color.green,Color.green,Color.green);
+		}
+	}
+	
+	public void onHitByBullet(HitByBulletEvent e) {
+		direction *= -1;
+		enemyLife += e.getPower()*3;
+	}
+	
+	public void onHitWall(HitWallEvent e) {
+		direction *= -1;
+	}
+	
+	public void onBulletHit(BulletHitEvent e) {
+		enemyLife -= 16;
+	}
+	
+	public void onWin(WinEvent e) {
+		System.out.println("Shrek is life.");
+		System.out.println("c,_.--.,y\n  7 a.a(\n (   ,_Y)\n :  '---;\n.'|.  - (\n");
+		setColors(Color.green,Color.green,Color.yellow);
+	}
 }
